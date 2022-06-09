73c41cc5911704445cfb3eded0a27c2953cf6427
Rrezon <rrezonhasani5@gmail.com>
2022-06-09 16:29:15 +0200

    added endpoint that creates and updates the data for a given key

--- src/cache/route.ts
+++ src/cache/route.ts
@@ -3,6 +3,7 @@ import {
 } from "express";
 import { logger } from "../utils/logger";
 import usecase from "./usecases";
+import { randomString } from "./usecases/cacheLimit";
 
 const router = Router();
 
@@ -32,20 +33,14 @@ router.get(
       if (data) {
         logger.info("Cache hit");
         message = "Cache found";
-        await usecase().update(data, {
-          value: data.value,
-          lastUsed: new Date(),
-        });
+        await usecase().update(data, { value: data.value, lastUsed: new Date() });
         return res.status(200).json({
           payload: data.get("value"),
           message,
         });
       }
       logger.info("Cache miss");
-      data = await usecase().create({
-        key,
-        value: "1",
-      });
+      data = await usecase().create({ key, value: "1" });
       message = "Cache created!";
       await usecase().cacheLimit();
       return res.status(201).json({
@@ -58,4 +53,33 @@ router.get(
   },
 );
 
+router.post(
+  "/:key",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { key } = req.params;
+      let data = await usecase().getByKey(key);
+      let message: string;
+      let statusCode: number;
+      if (data) {
+        logger.info("Cache updated");
+        await usecase().update(data, { value: randomString(), lastUsed: new Date() });
+        message = "Cache updated!";
+        statusCode = 200;
+      } else {
+        console.log("Cache created");
+        data = await usecase().create({ key, value: randomString() });
+        message = "Cache created!";
+        statusCode = 201;
+      }
+      await usecase().cacheLimit();
+      return res.status(statusCode).json({
+        payload: data.get("value"),
+        message,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 export default router;
