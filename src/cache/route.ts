3bb5b039ffc244350f5d6f3d4f0ce5d49d85186a
Rrezon <rrezonhasani5@gmail.com>
2022-06-09 16:21:18 +0200

    Add an endpoint that returns the cached data for a given key

--- src/cache/route.ts
+++ src/cache/route.ts
@@ -22,4 +22,40 @@ router.get(
   },
 );
 
+router.get(
+  "/:key",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { key } = req.params;
+      let data = await usecase().getByKey(key);
+      let message: string;
+      if (data) {
+        logger.info("Cache hit");
+        message = "Cache found";
+        await usecase().update(data, {
+          value: data.value,
+          lastUsed: new Date(),
+        });
+        return res.status(200).json({
+          payload: data.get("value"),
+          message,
+        });
+      }
+      logger.info("Cache miss");
+      data = await usecase().create({
+        key,
+        value: "1",
+      });
+      message = "Cache created!";
+      await usecase().cacheLimit();
+      return res.status(201).json({
+        payload: data.get("value"),
+        message,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
 export default router;
