0b0719a91f6dcd7f29299cf5ad8418d803744e6f
Rrezon <rrezonhasani5@gmail.com>
2022-06-09 15:36:43 +0200

    added cache router config in app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import cacheRouter from "./cache/route";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/cache", cacheRouter);
 
 export default app;
