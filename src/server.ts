a5c5d93624a5da252e448e50c488592a349485fd
Rrezon <rrezonhasani5@gmail.com>
2022-06-09 13:23:59 +0200

    added winston logger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import dotenv from "dotenv";
 import app from "./app";
+import { logger } from "./utils/logger";
 
 dotenv.config();
 
 async function main() {
   app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+    logger.info(`Server running on port ${process.env.PORT}`);
   });
 }
 
