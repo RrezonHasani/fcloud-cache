1665e74772d93037ee0db0da713efd0009e4e199
Rrezon <rrezonhasani5@gmail.com>
2022-06-09 15:18:29 +0200

    added functionality in usecase deleteByKey

---
+++ src/cache/usecases/deleteByKey.ts
@@ -0,0 +1,15 @@
+import { logger } from "../../utils/logger";
+import { Cache } from "../model";
+
+export default () => {
+  return async (key: string): Promise<boolean> => {
+    const cache = await Cache.findOne({ key });
+    if (!cache) {
+      logger.error(`Not found cache by key: ${key}`);
+      return false;
+    }
+    await cache.remove();
+    logger.info(`Deleted cache by key: ${key}`);
+    return true;
+  };
+};
--- src/cache/usecases/index.ts
+++ src/cache/usecases/index.ts
@@ -3,12 +3,14 @@ import getByKey from "./getByKey";
 import getAll from "./getAll";
 import create, { ICreateCache } from "./create";
 import update, { IUpdateCache } from "./update";
+import deleteByKey from "./deleteByKey";
 
 export interface ICacheUseCase {
   getByKey(key: string): Promise<ICache>
   getAll(): Promise<ICache[]>
   create(data: ICreateCache): Promise<ICache>
   update(entity: ICache, data: IUpdateCache): Promise<ICache>
+  deleteByKey(key: string): Promise<boolean>
 }
 
 export default (): ICacheUseCase => ({
@@ -16,4 +18,5 @@ export default (): ICacheUseCase => ({
   getAll: getAll(),
   create: create(),
   update: update(),
+  deleteByKey: deleteByKey(),
 });
